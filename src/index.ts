55caa07c2b9ab961e5c03eebe3a261eb11fec455
rym fr <ryymfradi@gmail.com>
2022-04-13 12:41:57 +0100

    webpack dev server config

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { formData } from "./forms";
+
+const form = document.querySelector('form')! ;
+
+form.addEventListener('submit' , e =>{
+    e.preventDefault() ;
+   const  data =  formData(form) ;
+   console.log(data);
+   
+})
+
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path') ;
+
+module.exports = {
+    devtool : 'eval-source-map',
+    entry : './src/index.ts' ,
+    module : {
+        rules :[
+            {
+                test : /\.ts$/ ,
+                include : [path.resolve(__dirname , 'src')],
+                use : 'ts-loader',
+            }
+        ]
+    }
+    ,
+    resolve :{
+        extensions : ['.ts','.js']
+    } ,
+    output : {
+        publicPath :'public',
+        filename : 'bundle.js',
+        path : path.resolve(__dirname , 'public')
+    }
+}
